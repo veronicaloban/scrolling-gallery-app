e1ea62c4d28b30b2813c9b8080613b49fec2b405
Veronica Loban <veronica.loban99@gmail.com>
2020-10-19 22:05:31 +0300

    new syntax checked

--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,42 @@
+let numberOfLoads = 1;
 let galleryElement = document.getElementById("gallery");
 
-let numberOfLoads = 1;
-
 //load data
-async function getDataFrom() {
+let getDataFromServer = async () => {
   let response = await fetch(`https://jsonplaceholder.typicode.com/photos?_page=${numberOfLoads++}&_limit=20`);
   let result = await response.json();
 
   return result;
 }
 
+
 //we create HTML Card elements based on the data loaded
 let createCardElement = (id, albumId, title, url, thumbnailUrl, parentElement) => {
   let cardElement = document.createElement("div");
-  cardElement.setAttribute("class", "card");
-  cardElement.setAttribute("id", id);
+  cardElement.classList.add('card');
+  cardElement.innerHTML =
+  `
+    <div class="card__image-container">
+      <img src="${thumbnailUrl}" alt="">
+    </div>
+    <p class="card__albumId">
+      ${albumId}
+    </p>
+    <p class="card__titleName">
+      ${title}
+    </p>
+  `;
 
-  let imageContainer = document.createElement("div");
-  imageContainer.setAttribute("class", "card__image-container");
-
-  let image = document.createElement("img");
-  image.setAttribute("src", thumbnailUrl);
-
-  let albumIdName = document.createElement("p");
-  albumIdName.setAttribute("class", "card__albumId");
-  albumIdName.innerHTML = `${albumId}`;
-
-  let titleName = document.createElement("p");
-  titleName.setAttribute("class", "card__titleName");
-  titleName.innerHTML = `${title}`;
-
-  cardElement.appendChild(imageContainer);
-  imageContainer.appendChild(image);
-  cardElement.appendChild(albumIdName);
-  cardElement.appendChild(titleName);
   parentElement.appendChild(cardElement);
 }
 
-getDataFrom().then(data => data.map(item => createCardElement(item.id, item.albumId, item.title, item.url, item.thumbnailUrl, galleryElement)))
-
+//load more content when needed
 let addMoreContent = () => {
     let windowBottom = document.documentElement.scrollTop + document.documentElement.clientHeight;
     windowBottom + 50 >= document.documentElement.scrollHeight ?
-    getDataFrom().then(data => data.map(item => createCardElement(item.id, item.albumId, item.title, item.url, item.thumbnailUrl, galleryElement))) : false;
+    getDataFromServer().then(data => data.map(item => createCardElement(item.id, item.albumId, item.title, item.url, item.thumbnailUrl, galleryElement))) : false;
   }
 
-
 window.addEventListener('scroll', addMoreContent);
+
+getDataFromServer().then(data => data.map(item => createCardElement(item.id, item.albumId, item.title, item.url, item.thumbnailUrl, galleryElement)))
